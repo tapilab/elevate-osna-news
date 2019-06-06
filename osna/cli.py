be92a3e6affade257f126318e9379843ba7c2dc3
Aron <aronwc@gmail.com>
2019-06-06 12:57:49 -0500

    web command

--- osna/cli.py
+++ osna/cli.py
@@ -14,5 +14,11 @@ def main(args=None):
     return 0
 
 
+@main.command('web')
+@click.option('-t', '--twitter-credentials', required=False, type=click.Path(exists=True), show_default=True, default=credentials_path, help='a json file of twitter tokens')
+def web(twitter_credentials):
+	from .app import app
+	app.run(host='0.0.0.0', debug=True)
+
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
