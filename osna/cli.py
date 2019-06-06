66dc1228ce69a40ebbf09cd5f61b950440e25d8f
Aron <aronwc@gmail.com>
2019-06-06 13:02:39 -0500

    test

--- osna/cli.py
+++ osna/cli.py
@@ -19,6 +19,7 @@ def main(args=None):
 def web(twitter_credentials):
 	from .app import app
 	app.run(host='0.0.0.0', debug=True)
+	
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
