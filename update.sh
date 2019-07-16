4f6e0450912fac228b6d2036c964e12a328cd836
Aron <aronwc@gmail.com>
2019-07-16 09:35:50 -0500

    tweak update script

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-git remote add template https://github.com/tapilab/elevate-osna-starter
+#git remote add template https://github.com/tapilab/elevate-osna-starter
 git fetch template
 git merge template/master --allow-unrelated-histories
 
