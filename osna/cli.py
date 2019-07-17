95d24f8d5e944888dca758277b3841e2868e34a2
Liu zhicheng <906834402@qq.com>
2019-07-17 16:18:46 -0500

    v3

--- osna/cli.py
+++ osna/cli.py
@@ -18,22 +18,21 @@ def main(args=None):
 @click.option('-p', '--port', required=False, default=5000, show_default=True, help='port of web server')
 def web(twitter_credentials, port):
 	from .app import app
-<<<<<<< HEAD
-	app.run(host='127.0.0.1', debug=True)
-	
-=======
+
 	app.run(host='127.0.0.1', debug=True, port=port)
 
 @main.command('stats')
 @click.argument('directory', type=click.Path(exists=True))
 def stats(directory):
-	"""
-	Read all files in this directory and its subdirectories and print statistics.
-	"""
-	print('reading from %s' % directory)
+    print('reading from %s' % directory)
+    import pandas as pd
+    import gzip
+    import json
+    df = pd.read_csv(directory+'/twitter.csv.gz')
+    print('Unique users: ' + str(df.social_id.nunique()))
+    print('Unique messages: ' + str(df.comment_tokens.nunique()))
 	# use glob to iterate all files matching desired pattern (e.g., .json files).
 	# recursively search subdirectories.
->>>>>>> 5ffe6275eb2a34deeecd9f88a5aa9373354e223b
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
