398560f43a3cf8a828ee9d7a8464574ecc9120e7
hx-Tang <tanghuaixi@bupt.edu.cn>
2019-07-21 20:02:33 -0500

    day3 lab ver1

--- osna/app/routes.py
+++ osna/app/routes.py
@@ -1,11 +1,13 @@
 from flask import render_template, flash, redirect, session
 from . import app
 from .forms import MyForm
-# from ..mytwitter import Twitter
+
 # from ..u import get_twitter_data, N_TWEETS
 from .. import credentials_path
 import sys
 
+from ..mytwitter import Twitter
+from .. import credentials_path
 
 # twapi = Twitter(credentials_path)
 
@@ -16,8 +18,16 @@ def index():
     result = None
     if form.validate_on_submit():
         input_field = form.input_field.data
+        tweets = get_tweets(input_field)
         flash(input_field)
-        return render_template('myform.html', title='', form=form)
+        return render_template('myform.html', title='', form=form, tweets=tweets)
     # return redirect('/index')
     return render_template('myform.html', title='', form=form)
+
+
+def get_tweets(name='twitterapi'):
+    tapi = Twitter(credentials_path)
+    tweetsj = tapi._get_tweets('screen_name', name, limit=200)
+    tweets = [t['full_text'] for t in tweetsj]
+    return tweets
 
