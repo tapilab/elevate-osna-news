57ac1560958b29b1b1d6939708127dc7af3f79fb
Haibara0517 <38417754+Haibara0517@users.noreply.github.com>
2019-07-30 14:14:14 -0500

    Get word matrix v1.0

---
+++ osna/get_matrix.py
@@ -0,0 +1,82 @@
+import glob,json
+import os,gzip
+import pandas as pd
+
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
+from nltk.corpus import stopwords
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def read_data(directory):
+    dfs = []
+    for label in ['real', 'fake']:
+        for file in glob.glob(directory + os.path.sep + label + os.path.sep + '*gz'):
+            # print('reading %s' % file)
+            df = pd.DataFrame((json.loads(line) for line in gzip.open(file)))
+            df['label'] = label
+            dfs.append(df)
+    df=pd.concat(dfs)[['publish_date', 'source', 'text', 'title', 'tweets', 'label']]
+    list_text = [i for i in list(df.text) if i != '']
+    df=df[df.text.isin(list_text)]
+    return df,list_text
+
+
+
+def tokennizer(s):
+    s = re.sub(r'http\S+', '', s)
+    s = re.sub(r'[0-9_\s]+', '', s)
+    s = re.sub(r"[^'\w]+", '', s)
+
+    s = re.compile(r"(?<=[a-zA-Z])'re").sub(' are', s)
+    s = re.compile(r"(?<=[a-zA-Z])'m").sub(' am', s)
+    s = re.compile(r"(?<=[a-zA-Z])'ve").sub(' have', s)
+    s = re.compile(r"(it|he|she|that|this|there|here|what|where|when|who|why|which)('s)").sub(r"\1 is", s)
+    s = re.sub(r"'s", "", s)
+    s = re.sub(r"can't", 'can not', s)
+    s = re.compile(r"(?<=[a-zA-Z])n't").sub(' not', s)
+    s = re.compile(r"(?<=[a-zA-Z])'ll").sub(' will', s)
+    s = re.compile(r"(?<=[a-zA-Z])'d").sub(' would', s)
+    return s
+
+
+def lemmatize(l):
+    wnl = WordNetLemmatizer()
+    for word, tag in pos_tag(l):
+        if tag.startswith('NN'):
+            yield wnl.lemmatize(word, pos='n')
+        elif tag.startswith('VB'):
+            yield wnl.lemmatize(word, pos='v')
+        elif tag.startswith('JJ'):
+            yield wnl.lemmatize(word, pos='a')
+        elif tag.startswith('R'):
+            yield wnl.lemmatize(word, pos='r')
+        else:
+            yield word
+
+def wordlist_train(list):
+    stopword=set(stopwords.words('english'))
+    list_new=[]
+    for l in list:
+        l=re.sub(r"[^\w']",' ',l).lower()
+        l1=[tokennizer(w) for w in l.split() if len(tokennizer(w))>2]
+        l=' '.join(l1)
+        l1=[tokennizer(w) for w in l.split() if len(tokennizer(w))>2 and tokennizer(w) not in stopword]
+        l=' '.join(lemmatize(l1))
+        if l!='':
+            list_new.append(l)
+    return list_new
+
+
+def get_matrix():
+    df, list_text = read_data('..\\training_data')
+    word_list=wordlist_train(list_text)
+    tv=TfidfVectorizer(min_df=2,max_df=1.0,ngram_range=(1,1),stop_words= 'english')
+    X=tv.fit_transform(word_list)
+    return X.toarray()
+
+
+
+if __name__=='__main__':
+    print(get_matrix())
