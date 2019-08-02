b0423690644337f3bf1b5fdb387d8ad09d815229
Haibara0517 <38417754+Haibara0517@users.noreply.github.com>
2019-08-02 03:06:18 -0500

    add get_desc() for user description

--- osna/get_wordlist.py
+++ osna/get_wordlist.py
@@ -1,9 +1,9 @@
 import re
+import numpy as np
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 from nltk.corpus import stopwords
-import numpy as np
-
+from nltk.tokenize import TweetTokenizer
 
 def tokennizer(s):
     s = re.sub(r'http\S+', '', s)
@@ -36,6 +36,23 @@ def lemmatize(l):
         else:
             yield word
 
+
+def tokennizer_desc(tknzr,sen):
+    list1=[]
+    for s in tknzr.tokenize(sen.lower()):
+        s=re.compile(r"(?<=[a-zA-Z])'re").sub(' are',s)
+        s=re.compile(r"(?<=[a-zA-Z])'m").sub(' am',s)
+        s=re.compile(r"(?<=[a-zA-Z])'ve").sub(' have',s)
+        s=re.compile(r"(it|he|she|that|this|there|here|what|where|when|who|why|which)('s)").sub(r"\1 is",s)
+        s=re.sub(r"'s","",s)
+        s=re.sub(r"can't",'can not',s)
+        s=re.compile(r"(?<=[a-zA-Z])n't").sub(' not',s)
+        s=re.compile(r"(?<=[a-zA-Z])'ll").sub(' will',s)
+        s=re.compile(r"(?<=[a-zA-Z])'d").sub(' would',s)
+        list1.append(s)
+    return(' '.join(list1))
+
+
 def get_text(list):
     stopword=set(stopwords.words('english'))
     list_new=[]
@@ -56,3 +73,9 @@ def get_source(list):
         l = ' '.join(l1)
         list_new.append(l)
     return list_new
+
+def get_desc(sentence):
+    tknzr = TweetTokenizer(strip_handles = True,reduce_len=True)
+    sen=tokennizer_desc(tknzr,sentence)
+    l1=[w for w in lemmatize((tknzr.tokenize(sen))) if w!='']
+    return(' '.join(l1))
