dc756d8f10f7a52ab04731a5f6f713aef092a1b2
Liu zhicheng <906834402@qq.com>
2019-08-05 10:36:08 -0500

    fix bug

--- osna/__init__.py
+++ osna/__init__.py
@@ -35,10 +35,10 @@ config_path = osna_path + 'osna.cfg'
 # twitter credentials.
 credentials_path = osna_path + os.path.sep +'credentials.json'
 # classifier
-clf_path = osna_path + 'clf.pkl'
-clf_path2 = osna_path + 'clf2.pkl'
-clf_path3 = osna_path + 'clf3.pkl'
-
+clf_path = osna_path + os.path.sep + 'clf.pkl'
+clf_path2 = osna_path + os.path.sep + 'clf2.pkl'
+clf_path3 = osna_path + os.path.sep + 'clf3.pkl'
+clf_path_ = osna_path + os.path.sep + 'clf_.pkl'
 # write default config if not present.
 if not os.path.isfile(config_path):
     write_default_config(config_path)
